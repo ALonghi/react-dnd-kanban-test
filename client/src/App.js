97226a79cdc793fdf28dc9f8eec0aa78e9620104
Alessio Longhi <alonghii@icloud.com>
2023-04-03 23:10:29 +0200

    Add frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Task from "./components/Task";
+
+function App() {
+	return (
+		<div className="w-[100vw] min-h-[100vh] h-full">
+			<Task />
+		</div>
+	);
+}
+
+export default App;
