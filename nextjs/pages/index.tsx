ec8484ebb54d60befb6a966ef70472d3eb7b3fa9
Alessio Longhi <alonghii@icloud.com>
2023-04-03 23:26:19 +0200

    Working with nextjs

---
+++ nextjs/pages/index.tsx
@@ -0,0 +1,11 @@
+import Task from "../components/Task"
+
+const IndexPage = () => (
+  <>
+    <div className="w-[100vw] min-h-[100vh] h-full">
+      <Task />
+    </div>
+  </>
+)
+
+export default IndexPage
