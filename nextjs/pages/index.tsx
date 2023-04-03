49ebc432a42701585ae3368dbf64f0526388d8c8
Alessio Longhi <alonghii@icloud.com>
2023-04-03 23:31:50 +0200

    Converted to typescript

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,8 +1,26 @@
 import Task from "../components/Task"
 
-const IndexPage = () => (
+export async function getServerSideProps() {
+  try {
+    const boards = await fetch("http://localhost:8080/api/boards")
+      .then((res) => res.json())
+      .then(res => res.data)
+    return {
+      props: {
+        boards
+      }
+    };
+  } catch (e) {
+    return {
+      props: {},
+    };
+  }
+}
+
+const IndexPage = ({ boards }) => (
   <>
     <div className="w-[100vw] min-h-[100vh] h-full">
+      <p>Found {boards?.length} boards</p>
       <Task />
     </div>
   </>
